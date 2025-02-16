c098af2f29bdbc929738255b0f227b8ada55f6fc
Sarai Rojas <sara.r.sandoval@gmail.com>
2025-02-16 00:49:14 -0600

    feat: run client and server with one commant

--- todo-list-app/src/components/Header/Header.jsx
+++ todo-list-app/src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
+'use client'
+
 import styles from './Header.module.css'
+import { useRouter } from 'next/navigation'
 
 const Header = () => {
+
+  const router = useRouter()
+
   return (
     <header className={styles.headerContainer}>
-      <p className={styles.signOut}>Sign out</p>
+      <p className={styles.signOut} onClick={() => router.push('/login')}>Sign out</p>
     </header>
   )
 }
